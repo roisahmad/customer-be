7956dd526ad01727b3d2dc7e2182c886e0137257
Ahmad Rois <ahmadlutfirois@gmail.com>
2025-05-03 18:36:04 +0700

    Update root api

--- app.js
+++ app.js
@@ -12,5 +12,9 @@ connectDB();
 app.use('/api/customers', require('./routes/customerRoute'));
 app.use('/api/analytics', require('./routes/analyticRoute'));
 
+app.get("/", (req, res) => {
+  res.send("API Service for Customer Data");
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
